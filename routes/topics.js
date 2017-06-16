0487950b09d4fc95934bd4b66a07e21e0dd176b1
Fauzi Kliman <fauzimerfoo@gmail.com>
2017-06-15 17:34:32 -0700

    Added basic categories route, added route for getting topic question, added eslint json file

--- routes/topics.js
+++ routes/topics.js
@@ -15,6 +15,61 @@ module.exports = (db) => {
         res.json({ content: "Hello topics!" });
     });
 
+    router.get("/:id", (req, res) => {
+        const topicId = req.params.id;
+
+        new Promise((resolve, reject) => {
+            db.collection(process.env.TOPICS_COLLECTION).findOne({ id: topicId }, (err, doc) => {
+                if (err || !doc) {
+                    reject(err);
+                }
+
+                else {
+                    resolve(doc);
+                }
+            });
+        }).then((topic) => {
+            const commentIds = topic.comments;
+
+            return new Promise((resolve, reject) => {
+                db.collection(process.env.COMMENTS_COLLECTION).find({ id: { $in: commentIds }}).toArray((err, docs) => {
+                    if (err || !docs) {
+                        reject(err);
+                    }
+
+                    else {
+                        const comments = docs.sort((a, b) => {
+                            if (a.postDate < b.postDate) {
+                                return 0;
+                            }
+
+                            else if (a.postDate > b.postDate) {
+                                return 1;
+                            }
+
+                            else {
+                                return 0;
+                            }
+                        });
+
+                        resolve({ topic, comments });
+                    }
+                });
+            });
+        }).then((data) => {
+            console.log(data);
+            res.json(data);
+            // res.json({ topic: data.topic });
+        }).catch((err) => {
+            console.log(err);
+            res.status(404).send();
+        });
+    });
+
+    router.get("/new", (req, res) => {
+        res.render("newTopics");
+    });
+
     router.post("/new", (req, res) => {
         const newTopic = {
             id: shortid.generate(),
